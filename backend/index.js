34c526bd5ca59f53dd5afae65adfc45d1055153c
Kevin yang <kevin7616@gmail.com>
2018-08-26 16:15:43 +1200

    Working demo

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,15 @@ const app = express();
 const path = require('path');
 const request = require('request');
 
-var content = fs.readFileSync('fuel.txt');
-var index = fs.readFileSync('index.html', 'utf8');
-var jsonContent = JSON.parse(content);
+app.use(express.static('..'));
 
-//app.get('/', (req, res) => res.send(jsonContent.data.milage));
-app.get('/', (req, res) => res.send(index));
 app.get('/fuel/:numberPlate', (req, res) => {
     request('http://resources.fuelsaver.govt.nz/ecca_tools_resources/webservices/webservice.php?service=fuel_label_generator&action=get_text&ref_type=plate&ref_value=' + req.params.numberPlate, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          const milage = JSON.parse(body).data.milage['value'].split(" ")[0];
-          const results = {milage: parseFloat(milage)}
+          const data = JSON.parse(body).data
+          const milage = data.milage.value.split(" ")[0];
+          const make = data.make.value;
+          const results = {milage: parseFloat(milage), make: make}
           res.send(JSON.stringify(results));
         }
     })
